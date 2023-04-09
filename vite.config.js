a46f82f5e39f250499875d3d7d02d0ee43d1ccc3
llvovll89 <svvvs5579@naver.com>
2023-04-10 00:03:14 +0900

    vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default {
+  plugins: [
+    react(),
+    reactRefresh(),
+    VitePluginPages({ pagesDir: 'src/pages' }),
+    VitePlugin404Page({ path: '/404.html' }),
+  ],
   base: '/movieapp/',
   build: {
     outDir: 'dist',
@@ -9,5 +16,4 @@ export default {
   optimizeDeps: {
     include: ['axios'],
   },
-  plugins: [reactRefresh(), VitePluginPages({ pagesDir: 'src/pages' }), VitePlugin404Page({ path: '/404.html' })],
 };
